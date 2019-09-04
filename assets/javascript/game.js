6b33b96a35c4ac95d43567ecd458f6054fca7d97
Nicole Roberts <nicolerobertsb@gmail.com>
2019-09-03 22:40:20 -0700

    added values for the crystals and console logged them

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,7 +5,7 @@ var sapphire;
 var diamond;
 var emerald;
 var userScore;
-var crystals = [ruby, sapphire, diamond, emerald];
+var crystalValue = [ruby, sapphire, diamond, emerald];
 var max = 120; // max value for the target score
 var min = 19;   // min value for the target score
 var maxCrystals = 12;   // max value for crystals
@@ -20,20 +20,39 @@ var loss = 0;
 // 3. generate random target score
 
 var targetScore = Math.floor(Math.random() * 120) + 1;
-console.log(targetScore);
+console.log("Random Number: " + targetScore);
 
-    // display random target score to the user
+// display random target score to the user
 
 $("#targetScoreSpot").text("Random Number: " + targetScore);
 
 // 3. set user score to 0
 
+userScore = 0;
 
-    // display user score on the web page
+// display user score on the web page
 
+$("#score").text(userScore);
 
 // 4. get value for each crystal
 
+for (var i = 0; i < crystalValue.length; i++) {
+    crystalValue[i] = Math.floor(Math.random() * maxCrystals) + minCrystals;
+
+    if (i === 0) {
+        console.log("Ruby Value: " + crystalValue[i]);
+    }
+    else if (i === 1) {
+        console.log("Sapphire Value: " + crystalValue[i]);
+    }
+    else if (i === 2) {
+        console.log("Diamond Value: " + crystalValue[i]);
+    }
+    else {
+        console.log("Emerald Value: " + crystalValue[i]);
+        console.log("-----------------");
+    }
+}
 
 // 5. when user clicks on a crystal, add crystal to user score
 
